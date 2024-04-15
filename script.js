4702fb1ec96f416417ba401ab2546af4852ad799
salgadojamesvan <151459528+salgadojamesvan@users.noreply.github.com>
2024-04-15 13:38:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let pass = document.getElementById("pass");
+let btn = document.getElementById("btn");
+let btncp = document.getElementById("btncp");
+
+function generatePass() {
+    let chars =
+        "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}?><";
+    let passLength = 20;
+    let password = "";
+
+    for (let i = 0; i < passLength; i++)
+        password += chars[Math.floor(Math.random() * chars.length)];
+
+    return password;
+}
+
+btn.addEventListener("click", () => {
+    pass.value = generatePass();
+});
+
+btncp.addEventListener("click", () => {
+    if (pass.value != "") {
+        navigator.clipboard.writeText(pass.value).then(function() {
+            pass.classList.add("active");
+            setTimeout(() => {
+                pass.classList.remove("active");
+            }, 1000);
+        }).catch(function(err) {
+            console.error('Could not copy text: ', err);
+        });
+    }
+});
